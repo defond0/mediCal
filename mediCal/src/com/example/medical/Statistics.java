68f76a5236dfb3cd775f8e76d8f2c189bac5dc55
Jeff DeFond <jeffdefond@gmail.com>
2014-11-10 10:34:10 -0800

    hw 5 code

--- mediCal/src/com/example/medical/Statistics.java
+++ mediCal/src/com/example/medical/Statistics.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.bluetooth.*;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.UUID;
 
 public class Statistics extends Activity {
@@ -33,11 +39,37 @@ public class Statistics extends Activity {
     private final BluetoothGattCallback btCb = new BluetoothGattCallback() {
     @Override
     public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic btchar){
-        System.out.println("Characteristic changed");
-        banner.setText(btchar.getStringValue(0));
+        System.out.println("CALLBACK");
+        c = btchar;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //int rfid = c.getIntValue(BluetoothGattCharacteristic.FORMAT_SINT16,0);
+                byte[] b = c.getValue();
+
+
+
+                String rfid= c.getStringValue(0);
+                int r = 100;
+                if(b[0]==(byte)0x12){
+                    rfid = "Pill 1";
+                    r=r*(-1);
+                }
+                else{
+                    rfid= "Pill 2";
+                    //rotate(100);
+                }
+                banner.setText(rfid);
+                rotate(r);
+
+
+            }
+        });
     }
 
+
     };
+
     private final UUID serviceUUID =    UUID.fromString("b0bb5820-5a0d-11e4-93ee-0002a5d5c51b");
 
     private final UUID rotateCharUUID = UUID.fromString("fb71bcc0-5a0c-11e4-91ae-0002a5d5c51b");
@@ -83,6 +115,17 @@ public class Statistics extends Activity {
         }
     };
 
+    public void rotate(int n){
+        final int q = n;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                writeCharacteristic((-1)*q);
+            }
+        },500);
+        writeCharacteristic(q);
+    }
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -118,19 +161,24 @@ public class Statistics extends Activity {
     public void submit(View v) {
         number = (EditText) findViewById(R.id.rotateText);
         String n = number.getText().toString();
-        System.out.println("We want to rotate by " +n);
+        int i = Integer.valueOf(n) ;
+        System.out.println(i);
+        System.out.println("We want to rotate by " +i);
         if( gatt.discoverServices()){
             System.out.println("Services Discovered");
-            boolean write=writeCharacteristic(n);
+            boolean write=writeCharacteristic(i);
             while(write==false){
-                write = this.writeCharacteristic(n);
+                write = this.writeCharacteristic(i);
             }
         }
 
 
     }
 
+
+
     public void listen(View v){
+        System.out.println("Now Setting Notifications on");
         if (gatt.discoverServices()) {
             boolean write = this.enableNotification();
             while (write == false) {
@@ -197,15 +245,17 @@ public class Statistics extends Activity {
             return false;
         }
         BluetoothGattDescriptor d = c.getDescriptor(notificationUUID);
-        if(d==null){
+        if(d ==null){
+            System.out.println("Null Descriptor");
             return false;
         }
+        gatt.setCharacteristicNotification(c, true);
         d.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
         boolean write = gatt.writeDescriptor(d);
-        return  write;
+        return write;
     }
 
-    public boolean writeCharacteristic(String n) {
+    public boolean writeCharacteristic(int n) {
         if (gatt == null||btAdapter==null) {
             System.out.println("no setup");
             return false;
@@ -220,14 +270,14 @@ public class Statistics extends Activity {
             System.out.println("Null Characteristic");
             return false;
         }
-        c.setValue(Character.toString((char) Integer.parseInt(n)));
+        c.setValue(n,BluetoothGattCharacteristic.FORMAT_SINT16,0);//Character.toString((char) Integer.parseInt(n)));
         boolean status = gatt.writeCharacteristic(c);
         System.out.println("Write Status "+ status);
         return status;
     }
 
 
-	@Override
+    @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.statistics, menu);
@@ -249,6 +299,7 @@ public class Statistics extends Activity {
     @Override
     public void onDestroy(){
         if (gatt!=null) {
+            gatt.disconnect();
             gatt.close();
             gatt = null;
         }
